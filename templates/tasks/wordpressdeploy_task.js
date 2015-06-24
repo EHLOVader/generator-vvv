422a1fce2e2e9b680ba34db349d1ae2186ca0ea3
Joseph Richardson <EHLOVader@users.noreply.github.com>
2015-06-24 16:44:17 -0400

    Added nicer deploy commands.

--- templates/tasks/wordpressdeploy_task.js
+++ templates/tasks/wordpressdeploy_task.js
@@ -1,3 +1,13 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-wordpress-deploy');
+
+    grunt.registerTask('deploy', function (target) {
+        if (target == null) {
+            grunt.warn('deploy target must be specified, like deploy:staging.');
+        }
+        grunt.option('target', target);
+        grunt.task.run.apply(grunt.task, ['push_files', 'push_db', 'create_config']);
+
+    });
+
 }
